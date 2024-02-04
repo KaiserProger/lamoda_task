80068ea3c67efc30f0f17491999360d94bb1e993
KaiserProger <kaisergrobe@gmail.com>
2024-02-05 01:50:12 +0700

    Add goose migration

---
+++ migrations/20240204184817_init_migration.sql
@@ -0,0 +1,36 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE items(
+    code int PRIMARY KEY,
+    name string,
+    size int
+);
+
+CREATE TABLE warehouses(
+    id int PRIMARY KEY,
+    name string,
+    accessible bool
+);
+
+CREATE TABLE warehouses_items(
+    item_code int REFERENCES items(code),
+    warehouse_id int REFERENCES warehouses(id),
+    quantity int,
+    PRIMARY KEY (item_code, warehouse_id)
+);
+
+CREATE TABLE reservations(
+    item_code int REFERENCES items(code),
+    warehouse_id int REFERENCES warehouses(id),
+    quantity int,
+    PRIMARY KEY (item_code, warehouse_id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE reservations;
+DROP TABLE warehouses_items;
+DROP TABLE warehouses;
+DROP TABLE items;
+-- +goose StatementEnd
