6484564e666c5ecd1d0053ceb3de5df61bc876c0
KaiserProger <kaisergrobe@gmail.com>
2024-02-08 18:48:44 +0700

    Add FreeReservation tests

--- internal/app/services/item_service_test.go
+++ internal/app/services/item_service_test.go
@@ -153,9 +153,99 @@ func TestItemService_MakeReservation(t *testing.T) {
 func TestItemService_FreeReservation(t *testing.T) {
 	t.Parallel()
 
-	svc, txManager, itemRepo, whRepo, reserveRepo := fixture(t)
+	svc, txManager, _, whRepo, reserveRepo := fixture(t)
 
-	tests := []test{}
+	tests := []test{
+		{
+			name: "no reservations",
+			data: []int{1, 2, 3, 4},
+			mock: func() {
+				txManager.EXPECT().WithinTransaction(context.Background(), gomock.Any()).DoAndReturn(func(ctx context.Context, f func(ctx context.Context) error) error {
+					return f(ctx)
+				}).AnyTimes()
+				reserveRepo.EXPECT().GetReservation(context.Background(), gomock.Any()).Return(nil, nil)
+			},
+			result: nil,
+			err:    appErrors.ErrNotFound,
+		},
+		{
+			name: "some items not reserved",
+			data: []int{1, 1, 1, 1, 1, 2, 2, 2, 4, 4, 7},
+			mock: func() {
+				items := []*models.ReservationItem{
+					{
+						ItemCode:    1,
+						WarehouseId: 1,
+						Quantity:    4,
+					},
+					{
+						ItemCode:    2,
+						WarehouseId: 1,
+						Quantity:    3,
+					},
+					{
+						ItemCode:    4,
+						WarehouseId: 2,
+						Quantity:    1,
+					},
+					{
+						ItemCode:    7,
+						WarehouseId: 6,
+						Quantity:    2,
+					},
+				}
+				txManager.EXPECT().WithinTransaction(context.Background(), gomock.Any()).DoAndReturn(func(ctx context.Context, f func(ctx context.Context) error) error {
+					return f(ctx)
+				}).AnyTimes()
+				reserveRepo.EXPECT().GetReservation(context.Background(), map[int]int{
+					1: 5,
+					2: 3,
+					4: 2,
+					7: 1}).Return(items, nil).AnyTimes()
+			},
+			result: nil,
+			err:    appErrors.ErrImpossibleReserve,
+		},
+		{
+			name: "success",
+			data: []int{1, 1, 1, 1, 1, 2, 2, 2, 4, 4, 7},
+			mock: func() {
+				items := []*models.ReservationItem{
+					{
+						ItemCode:    1,
+						WarehouseId: 1,
+						Quantity:    5,
+					},
+					{
+						ItemCode:    2,
+						WarehouseId: 1,
+						Quantity:    3,
+					},
+					{
+						ItemCode:    4,
+						WarehouseId: 2,
+						Quantity:    2,
+					},
+					{
+						ItemCode:    7,
+						WarehouseId: 6,
+						Quantity:    2,
+					},
+				}
+				txManager.EXPECT().WithinTransaction(context.Background(), gomock.Any()).DoAndReturn(func(ctx context.Context, f func(ctx context.Context) error) error {
+					return f(ctx)
+				}).AnyTimes()
+				reserveRepo.EXPECT().GetReservation(context.Background(), map[int]int{
+					1: 5,
+					2: 3,
+					4: 2,
+					7: 1}).Return(items, nil).AnyTimes()
+				reserveRepo.EXPECT().FreeReservation(context.Background(), items).Return(nil)
+				whRepo.EXPECT().AddToStock(context.Background(), items).Return(nil)
+			},
+			result: nil,
+			err:    nil,
+		}}
 
 	for _, tt := range tests {
 		tt := tt
